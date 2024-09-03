c56202bd699d07d95d07fbe91aa26c001cc4071a
Yogesh <pantyogesh992@gmail.com>
2024-09-03 17:33:23 +0530

    Add files via upload

---
+++ If_else/helloworld.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+ int x,y;
+ printf("Enter the number = ");
+ scanf("%d", & x );
+ printf("Enter the number = ");
+ scanf("%d", & y);
+ if (x>y)
+ {
+  printf("x is greater than y");
+ }
+ else 
+ {
+    printf("x is less than y");
+ }
+ return 0 ;
+}
